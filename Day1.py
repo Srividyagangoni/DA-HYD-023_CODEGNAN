539faa4ef6166a0a0468587d1fa32ebf9f442f48
Srividyagangoni <srividyagangoni01@gmail.com>
2026-07-28 13:35:55 +0530

    Tokens

---
+++ Day1.py
@@ -0,0 +1,42 @@
+#IDLE is color coding environment
+#hastag is used as single line comment
+'''
+Tokens-->A smallest unit in a program
+5 Types are:
+Keywords
+Identifiers
+Literals
+Variables
+Operators
+Punctuators
+'''
+
+name="codegnan"
+print(name)
+
+email_id="srividyagangoni01@gmail.com"
+print(email_id)
+
+#1name="vidya"
+#print(1name)  #Special char and numbers are not used at first and no keywords are used
+
+#variable -->named memory location where we stor the data, its a placeholder
+batch="cse"
+print(batch)
+
+#Literals--> These are constants (value(s)) -->number, cahracter
+
+#Operators-->Operators perform specific operation-->7 types
+
+length=5
+breadth=3
+area=length*breadth
+print(area)
+
+
+#Multiassignment of variables
+
+name,location,age="codegnan","hyd",7
+print(name)
+print(location)
+print(age)
